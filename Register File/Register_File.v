4789a922cdb66fb1413e04ad9993ed638cf78ebe
HayatGamal2000 <71998574+HayatGamal2000@users.noreply.github.com>
2022-10-04 12:03:25 +0200

    Update Register_File.v

--- Register File/Register_File.v
+++ Register File/Register_File.v
@@ -1,11 +1,11 @@
 module Register_File (
-  input   wire [15:0]     WrData,
+  input   wire [7:0]     WrData,
   input   wire [2:0]      Address,
   input   wire            WrEn,
   input   wire            RdEn,
   input   wire            CLK,
   input   wire            RST,
-  output  reg  [15:0]     RdData
+  output  reg  [7:0]     RdData
   );
   
   reg [7:0] mem[0:15];
@@ -17,7 +17,7 @@ module Register_File (
       begin
       for(i=0 ; i<16 ; i=i+1)
       begin
-        mem [i] <= 16'b0;
+        mem [i] <= 8'b0;
       end
     end
   else
